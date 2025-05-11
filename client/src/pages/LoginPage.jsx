82080a725e64daaae8dcf86dc174f5ac868c959d
Balraj <baludeveloper06@gmail.com>
2025-05-11 07:03:25 +0530

    final

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -224,15 +224,6 @@ export default function LoginPage() {
               Sign in with Google
             </Button>
 
-            <Button
-              fullWidth
-              variant="outlined"
-              onClick={() => alert('Sign in with Facebook')}
-              startIcon={<FacebookIcon />}
-            >
-              Sign in with Facebook
-            </Button>
-
             <Typography sx={{ textAlign: 'center' }}>
               Don&apos;t have an account?{' '}
               <Link href="/signup" variant="body2">
